c977c65c0c0a04c6a6903e06ac3a1e9d308b9434
Joakim S. Hovlandsvåg <joakim.hovlandsvag@gmail.com>
2015-05-04 08:11:24 +0200

    Change to use only a single JSON import file from Doit

--- import_doit_json.py
+++ import_doit_json.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+""" Script for exporting Doit.im data into Todoist. """
 
 import argparse
 try:
@@ -7,7 +8,6 @@ try:
 except ImportError:
     import json
     SIMPLEJSON = False
-
 
 
 def parse_json_file(filename):
@@ -22,20 +22,24 @@ def parse_json_file(filename):
         j = simplejson.loads
     else:
         j = json.loads
+    # TODO: Handle the exception that the file is still in HTML and not properly
+    # modified for pure JSON
     return j(raw)
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('projects', help='The projects file, in JSON format')
-    parser.add_argument('tasks', help='The tasks file, in JSON format')
+    parser = argparse.ArgumentParser(description="Import Doit.im data and "
+                                                 "export it to Todoist")
+    parser.add_argument('doit_file',
+                        help='The file with data from Doit.im, in JSON format')
     args = parser.parse_args()
 
-    projects = parse_json_file(args.projects)
-    print "%d projects found" % len(projects)
-    print projects[0]
-    tasks = parse_json_file(args.tasks)
-    print "%d tasks found" % len(tasks)
-    print tasks[0]
+    doit_data = parse_json_file(args.doit_file)
+    print "Doit.im data read. Status:"
+    for k in doit_data:
+        print "%15s: %5d elements" % (k, len(doit_data[k]))
+
+    for p in doit_data['tags']:
+        print p['name']
 
 if __name__ == '__main__':
     main()
