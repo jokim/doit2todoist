461dd30b9a912af27d8f402c01112157849216bc
Joakim S. Hovlandsvåg <joakim.hovlandsvag@gmail.com>
2015-05-04 09:37:19 +0200

    Add class for administering the Doit data

--- import_doit_json.py
+++ import_doit_json.py
@@ -26,6 +26,40 @@ def parse_json_file(filename):
     # modified for pure JSON
     return j(raw)
 
+class Doit:
+
+    """ A representation of the data from Doit.im. """
+
+    def __init__(self, doit_data):
+        self._doit_data = doit_data
+
+        cl = self._cleanup
+        self.tasks = dict((t['id'], cl(t)) for t in doit_data['tasks'])
+        self.tags = dict((t['uuid'], cl(t)) for t in doit_data['tags'])
+        self.contexts = dict((t['uuid'], cl(t)) for t in doit_data['contexts'])
+        self.projects = dict((t['uuid'], cl(t)) for t in doit_data['projects'])
+        # self.contacts not tested yet
+
+    def _cleanup(self, item):
+        """Do clean up on a given item and return it prettified.
+
+        Some elements in the various items are a bit messy. For instance could
+        names and titles have newlines in them, probably due to the conversion
+        through JSON.
+
+        """
+        ret = item.copy()
+        # Remove newlines
+        for k in ('name', 'title'):
+            if k in ret:
+                ret[k] = ret[k].replace('\n', '')
+        return ret
+
+    def print_status(self):
+        """Print status on the Doit content."""
+        for k in ('tasks', 'projects', 'tags', 'contexts'):
+            print "%15d %s" % (len(getattr(self, k)), k)
+
 def main():
     parser = argparse.ArgumentParser(description="Import Doit.im data and "
                                                  "export it to Todoist")
@@ -34,12 +68,10 @@ def main():
     args = parser.parse_args()
 
     doit_data = parse_json_file(args.doit_file)
-    print "Doit.im data read. Status:"
-    for k in doit_data:
-        print "%15s: %5d elements" % (k, len(doit_data[k]))
+    doit = Doit(doit_data)
 
-    for p in doit_data['tags']:
-        print p['name']
+    print "Doit.im data read. Status:"
+    doit.print_status()
 
 if __name__ == '__main__':
     main()
