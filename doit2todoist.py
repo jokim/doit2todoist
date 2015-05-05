4a75ae58c069ee6d02ec7a76c8056653ede7ce96
Joakim S. Hovlandsvåg <joakim.hovlandsvag@gmail.com>
2015-05-05 11:12:32 +0200

    Start communication with Todoist and export labels

--- doit2todoist.py
+++ doit2todoist.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 """ Script for exporting Doit.im data into Todoist. """
 
+import sys
 import argparse
+import todoist
 try:
     import simplejson
     SIMPLEJSON = True
@@ -68,11 +70,63 @@ class Doit:
         """
         return self.projects[uuid]['name']
 
+    def list_context_names(self):
+        """Return a list of all the contexts, by its names."""
+        return dict((c['name'], c) for c in self.contexts.itervalues())
+
+    def list_tag_names(self):
+        """Return a list of all the tags, by its names."""
+        return dict((t['name'], t) for t in self.tags.itervalues())
+
+class Todoist_exporter:
+
+    """ Class that handles the export to Todoist. """
+
+    def __init__(self, doit, tdst):
+        self.doit = doit
+        self.tdst = tdst
+
+    def export(self):
+        """Do the full export to Todoist"""
+
+        self.export_labels()
+        # TODO:
+        # self.export_projects()
+        # TODO: inbox is special
+        # self.export_tasks()
+        self.tdst.commit()
+        # TODO: Ask for confirmation before commit
+        self.tdst.sync()
+
+    def export_labels(self):
+        """Export all labels to Todoist.
+
+        It fetches Contexts and Tags from Doit and transforms them into
+        Todoist's Labels.
+
+        Some name conversion is done.
+
+        """
+        names = self.doit.list_context_names().keys()
+        names += self.doit.list_tag_names().keys()
+        print "All names:", names
+
+        self.tdst.labels.sync()
+        existing = [l['name'] for l in self.tdst.labels.all()]
+
+        for name in names:
+            if name in existing:
+                continue
+            print "Creating label: %s" % name
+            self.tdst.labels.add(name)
+
 def main():
     parser = argparse.ArgumentParser(description="Import Doit.im data and "
                                                  "export it to Todoist")
     parser.add_argument('doit_file',
                         help='The file with data from Doit.im, in JSON format')
+    parser.add_argument('apikey',
+                        help='Your API key for your account in Todoist')
     args = parser.parse_args()
 
     doit_data = parse_json_file(args.doit_file)
@@ -81,5 +135,17 @@ def main():
     print "Doit.im data read:"
     doit.print_status()
 
+    tdst = todoist.TodoistAPI(args.apikey)
+    status = tdst.sync()
+    if 'error' in status:
+        print "Error from Todoist: %s" % status['error']
+        return 1
+    exp = Todoist_exporter(doit, tdst)
+    print "Exporting to Todoist..."
+    exp.export()
+    print "export done!"
+
+    return 0
+
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
